3ef544540aed89fc5dc8cd84ef8578aba5b5042e
bhumika-chaudhari <bhumikasc10@gmail.com>
2026-07-09 02:11:31 +0530

    Connect backend to MySQL and create asset route

--- server/app.js
+++ server/app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to database
+require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const assetRoutes = require("./routes/assetRoutes");
+
+app.use("/api/assets", assetRoutes);
 app.get("/", (req, res) => {
-  res.json({
-    message: "Asset Management API is running 🚀",
-  });
+    res.json({
+        message: "Asset Management API is running 🚀"
+    });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
